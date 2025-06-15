788d953f27455e57676423155da2a7b27765d5b4
RANKblank36 <tendosana@gmail.com>
2025-06-15 17:55:15 +0200

    Add files via upload

--- from tkinter import.py
+++ from tkinter import.py
@@ -1,43 +1,88 @@
+# Import necessary libraries
+
 from tkinter import *
 
+# Create Window
 
 root = Tk()
+
 root.title('Login App')
+
 root.geometry('400x400')
+
+# Create a frame to organize elements better
 
 frame = Frame(master=root, height=200, width=360, bg="#d0efff")
 
+# Add widgets
+
+# Add Label for Full Name
 
 lbl1 = Label(frame, text="Full Name", bg="#3895D3", fg='white', width=12)
 
+# Add Label for Email Id
+
 lbl2 = Label(frame, text="Email Id", bg="#3895D3", fg='white', width=12)
+
+# Add Label for Enter Password
+
+lbl3 = Label(frame, text="Enter Password", bg="#3895D3", fg='white', width=12)
+
+# Use Entry Widget to create a text box for user to enter details
+
 name_entry = Entry(frame)
+
 email_entry = Entry(frame)
-pass_entry = Entry(frame, show="*")
+
+pass_entry = Entry(frame, show="*") # 'show="*" hides the password characters
+
+# Function to display message after account creation
 
 def display():
-    
-    name = name_entry.get()
 
-    greet = "Hey " + name
+# Get the name entered by the user
 
-    message = "\nCongratulations for your new account!"
+      name = name_entry.get()
 
-    textbox.insert(END, greet)
+# Create a greeting message
 
-    textbox.insert(END, message)
+      greet = "Hey " + name
 
-    textbox = Text(bg="#BEBEBE", fg="black")
+# Create a congratulatory message
+
+      message = "\nCongratulations for your new account!"
+
+# Insert the greeting into the textbox
+
+      textbox.insert(END, greet)
+
+# Insert the congratulatory message into the textbox
+
+      textbox.insert(END, message)
+
+textbox = Text(bg="#BEBEBE", fg="black")
+
 btn = Button(text="Create Account", command=display, bg="red")
+
 frame.place(x=20, y=0)
+
 lbl1.place(x=20, y=20)
+
 name_entry.place(x=150, y=20)
 
+# Place the Email Id label and entry field
+
 lbl2.place(x=20, y=80)
+
 email_entry.place(x=150, y=80)
 
+# Place the Password label and entry field
+
 lbl3.place(x=20, y=140)
+
 pass_entry.place(x=150, y=140)
+
+# Place the Create Account button
 
 btn.place(x=130, y=210)
 
